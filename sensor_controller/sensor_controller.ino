1314d60a5518c6d2d92eb0f742cfb6e59d0b406e
Jon Skjelsbæk <skjelsbek@hotmail.com>
2018-11-09 19:20:21 +0100

    sensor_controller done

--- sensor_controller/sensor_controller.ino
+++ sensor_controller/sensor_controller.ino
@@ -1,23 +1,24 @@
 #include <Ultrasonic.h>
 #include "Stepper.h"
 
-#define trig_front  6 // Front sensor trigger
-#define echo_front  7 // Front sensor echo
-#define trig_rear   8 // Rear sensor trigger
-#define echo_rear   9 // Rear sensor echo
-#define turn_left   10 // Output signal telling vehicle to turn left
-#define turn_right  11 // Output signal telling vehicle to turn right
-#define reverse     12 // Output signal telling vehicle to start backing
-#define stuck       13 // Output signal telling vehicle that it's stuck and has to stop
-#define feedback    18 // Feedback from Micro:bit when it's done turning
+#define trig_front    6 // Front sensor trigger
+#define echo_front    7 // Front sensor echo
+#define trig_rear     8 // Rear sensor trigger
+#define echo_rear     9 // Rear sensor echo
+#define turn_left     10 // Output signal telling vehicle to turn left
+#define turn_right    11 // Output signal telling vehicle to turn right
+#define reverse       12 // Output signal telling vehicle to start backing
+#define stuck         13 // Output signal telling vehicle that it's stuck and has to stop
+#define feedback_in   18 // Feedback from Micro:bit when it's done turning
+#define feedback_out  19  // Feedback to Micro:bit
 
 Ultrasonic sensor_front(trig_front, echo_front);  // Sensor facing forward
 Ultrasonic sensor_rear(trig_rear, echo_rear);     // Sensor facing backward
 
 unsigned int distance_front;  // Measured distance to obstacle in front of vehicle
 unsigned int distance_rear;   // Measured distance to obstacle behind vehicle
-unsigned int distance_left;
-unsigned int distance_right;
+unsigned int distance_left;   // Measured distance to obstacle to the left of vehicle
+unsigned int distance_right;  // Measured distance to obstacle to the right of vehicle
 
 // Inputs on stepper motor
 #define IN1 2
@@ -34,6 +35,7 @@ unsigned long time_last = millis(); // Used for periodically updating and checki
 
 void setup()
 {
+  Serial.begin(9600);
   pinMode(turn_left, OUTPUT);
   digitalWrite(turn_left, LOW);
   
@@ -46,16 +48,21 @@ void setup()
   pinMode(stuck, OUTPUT);
   digitalWrite(stuck, LOW);
 
+  pinMode(feedback_out, OUTPUT);
+  digitalWrite(feedback_out, HIGH);
+
   stepper.setSpeed(1100);
 
-  // Attach interrupt on feedback to interrupt service routine isr
-  pinMode(feedback, INPUT);  
-  attachInterrupt(digitalPinToInterrupt(feedback), isr, FALLING);
+  // Attach interrupt on feedback_in to interrupt service routine isr
+  pinMode(feedback_in, INPUT);  
+  attachInterrupt(digitalPinToInterrupt(feedback_in), isr, FALLING);
 }
 
 // interrupt service routine setting halt to false
 void isr()
-{
+{  
+  digitalWrite(turn_left, LOW);
+  digitalWrite(turn_right, LOW);
   halt = false;
 }
 
@@ -64,37 +71,41 @@ void loop()
   // the loop does nothing if halt = true
   if (!halt)
   { 
+    digitalWrite(feedback_out, LOW);
     // dir = false -> moving forward routine, dir = true -> reverse routine
     if (!dir)
     {
       distance_front = sensor_front.distanceRead(); // Read sensor data from sensor_front
 
-      // Check if distance_front < 50 cm, if not, do nothing
-      if (distance_front <= 50)
+      // Check if distance_front < 70 cm, if not, do nothing
+      if (distance_front <= 70 && distance_front != 0)
       {
         digitalWrite(stuck, HIGH); // Set stuck signal high for Micro:bit to temporary stop
         distance_rear = sensor_rear.distanceRead(); // Read sensor data from sensor_rear
-        stepper.step(512); // Turn stepper motor 90deg to be able to check distance on the sides
-
+        stepper.step(512); // Turn stepper motor 90deg to be able to check distance on the sides              
+        
         // Stepper motor rotated clockwise, so the front sensor is now, and rear sensor is left
         distance_right = sensor_front.distanceRead(); // Read right distance
         distance_left = sensor_rear.distanceRead();   // Read left distance
 
         // Obstacle on both sides -> check if it is possible to back up, else stuck forever.
-        if (distance_right <= 50 && distance_left <= 50)
+        if (distance_right <= 70 && distance_right != 0 && distance_left <= 70 && distance_left != 0)
         {      
-          if (distance_rear > 50)
+          if (distance_rear > 70 || distance_rear == 0)
           {
-            digitalWrite(reverse, HIGH); // Tell Micro:bit to back up
             digitalWrite(stuck, LOW); // Letting Micro:bit know we're no longer stuck
+            digitalWrite(reverse, HIGH); // Tell Micro:bit to back up            
             dir = true; // Setting direction to reverse
-          }    
+          }
+          stepper.step(-512);
         }
         // If one or both of the sides are clear, set the corresponding turn signal high,
         // let Micro:bit know we're no longer stuck, rotate stepper back to origin
-        // and halt until feedback from Micro:bit goes low
+        // and halt until feedback_in from Micro:bit goes low
         else  
         {
+          digitalWrite(stuck, LOW);
+          
           if (distance_right > distance_left)
           {
             digitalWrite(turn_right, HIGH);  
@@ -102,11 +113,11 @@ void loop()
           else
           {
             digitalWrite(turn_left, HIGH); 
-          }      
-          
-          digitalWrite(stuck, LOW);
+          }  
+                                  
           stepper.step(-512);
-          halt = true;  
+          digitalWrite(feedback_out, HIGH);
+          halt = true;                    
         }
       } 
     }
@@ -119,7 +130,7 @@ void loop()
       distance_left = sensor_rear.distanceRead();
 
       // Checking if either left or right is clear
-      if (distance_right > 50 || distance_left > 50)
+      if (distance_right > 70 || distance_right == 0 || distance_left > 70 || distance_left == 0)
       {
         // Setting the turn direction
         if (distance_right > distance_left)
@@ -130,28 +141,31 @@ void loop()
         {
           digitalWrite(turn_left, HIGH); 
         }
-                       
-        stepper.step(-512); // Turning the stepper motor back to origin
-        halt = true;        // halt = true -> wait for feedback signal to go low
-        dir = false;        //direction = false -> forward
+                               
+        stepper.step(-512);         // Turning the stepper motor back to origin
+        digitalWrite(reverse, LOW); // Tell Micro:bit to no longer back up
+        dir = false;                //direction = false -> forward
+        digitalWrite(feedback_out, HIGH);
+        halt = true;                // halt = true -> wait for feedback_in signal to go low     
       }
       // If time since last time rear was checked is more than 2 sec
-      else if (millis() - time_last >= 2000)
+      else if (millis() - time_last >= 1000)
       {
         stepper.step(-512); // Turn stepper motor back to origin
         distance_rear = sensor_rear.distanceRead(); // Measure rear distance
         
-        if (distance_rear <= 50)
+        if (distance_rear <= 70 && distance_rear != 0)
         {
-          // If you end up here you're fucked!
           digitalWrite(stuck, HIGH);
+          digitalWrite(reverse, LOW);
         }
         else
         {
           // Rear is still clear, keep backing up
-          stepper.step(512); // Turn stepper motor so sensors are facing the sides
-          time_last = millis(); // Update the variable keeping track of the last time rear was checked
+          stepper.step(512); // Turn stepper motor so sensors are facing the sides          
         }
+
+        time_last = millis(); // Update the variable keeping track of the last time rear was checked
       }
     }
   }    
